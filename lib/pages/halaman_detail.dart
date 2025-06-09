6974f2833b60a052dd8fa7a5729499ee2656ae6e
jona354 <jonasyaifudin@gmail.com>
2025-06-09 21:28:04 +0700

    menambahkan foto dan, memperbaiki posisi iconbottom

--- lib/pages/halaman_detail.dart
+++ lib/pages/halaman_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main(List<String> args) {
   runApp(MaterialApp(
     home: DetailBerita(),
@@ -17,14 +18,25 @@ class _DetailBeritaState extends State<DetailBerita> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: Column(
-        children: [
-          Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVweX5kUAgN-_FgMV9zJQ4D39l7EEgGd59Pg&s",
-          width: double.infinity,
-          height: 250,
-          fit: BoxFit.cover,)
-        ],
-      )),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+              child: IconButton(
+                onPressed: () {},icon: Icon(Icons.arrow_back),iconSize: 30,
+              ),
+            ),
+            Image.network(
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVweX5kUAgN-_FgMV9zJQ4D39l7EEgGd59Pg&s",
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
